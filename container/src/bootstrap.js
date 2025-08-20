b6965d76c96ee8ce861e87445062cb3412e52ba6
Gaby Carbajal <galvarezc@proveedor.intercorp.com.pe>
2025-08-20 15:35:21 -0500

    chore: handling mount products when it is running through container or in isolation

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,6 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartIndex';
 
 document.querySelector('#container').innerHTML = `<h1>Soy el fucking container</h1>`
+
+mount(document.querySelector('#mf-products'))
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,36 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+    let products = '';
 
-for (let i = 1; i <= 10; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
+    for (let i = 1; i <= 10; i++) {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+
+    el.innerHTML = products;
 }
 
-document.querySelector('#mf-products').innerHTML = products;
+/**
+ * Context/Situation #1
+ * We are running this file in development in isolation
+ * We are using our local index.html file
+ * Which DEFINITELY has an element with an id of 'local-products'
+ * We want to immediately render or app into that element
+ */
+if (process.env.NODE_ENV === 'development') {
+    const el = document.querySelector('#local-products');
+    if (el) {
+        mount(el)
+    }
+}
+
+/**
+ * Context/Situation #2
+ * We are running this file in development or production mode
+ * through the CONTAINER app
+ * NO GUARANTEE that an element with an id of 'local-products' exists
+ * WE DO NOT WANT try to immediately render the app
+ */
+
+export { mount }
