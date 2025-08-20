f89c63ae793230f64bae11fdd3a0a33ebefa7be9
Gaby Carbajal <galvarezc@proveedor.intercorp.com.pe>
2025-08-20 12:10:29 -0500

    feat: share depencies between mfs, but it breaks isolation eager deployment

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
       filename: 'remoteEntry.js',
       exposes: {
         './CartIndex': './src/index'
-      }
+      },
+      shared: ['faker']
     }),
     new HtmlPlugin({
       template: './public/index.html',
--- products/webpack.config.js
+++ products/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
         filename: 'remoteEntry.js',
         exposes: {
           './ProductsIndex': './src/index'
-        }
+        },
+        shared: ['faker']
       }),
       new htmlPlugin({
           template: './public/index.html',
